0fb3db61aa00b680933cd268d4efe65e510e047a
Milan992 <mitic.gsm@gmail.com>
2020-06-29 13:59:53 +0200

    implemented printing method

--- DAN_XLI_Milan_Mitic/PrintApp/MainWindow.xaml.cs
+++ DAN_XLI_Milan_Mitic/PrintApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,54 @@ namespace PrintApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string text = "";
+        static int numberOfCopies = 1;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numberOfCopies; i++)
+            {
+                string location = (i + 1).ToString() +"." + DateTime.Now.ToString("dd_MM_yyyy_hh_mm");
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(@"..\..\" + location + ".txt"))
+                    {
+                        sw.Write(text);
+                    }
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString());
+                }
+            }
+        }
+
+        private void TxtText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox objTextBox = (TextBox)sender;
+            text = objTextBox.Text;
+        }
+
+        private void TxtNumberOfCopies_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox objTextBox = (TextBox)sender;
+            int.TryParse(objTextBox.Text, out numberOfCopies);
+        }
+
+        private void PbProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
